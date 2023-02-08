e4e9a48e97374384f6df5bde3b2207c77e623f52
Mohsen77sk <Mohsen77sk@gmail.com>
2023-02-08 17:17:41 +0330

    Fix test app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-import { AppComponent } from './app.component';
 import { NgxTouchKeyboardModule } from 'ngx-touch-keyboard';
 
+import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -20,6 +24,7 @@ describe('AppComponent', () => {
         MatInputModule,
         MatSelectModule,
         MatButtonModule,
+        MatDialogModule,
         MatTooltipModule,
         MatToolbarModule,
         MatFormFieldModule,
@@ -27,11 +32,12 @@ describe('AppComponent', () => {
       ],
       declarations: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
--- src/app/material-dialog-example/material-dialog-example.component.spec.ts
+++ src/app/material-dialog-example/material-dialog-example.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgxTouchKeyboardModule } from 'ngx-touch-keyboard';
 
 import { MaterialDialogExampleComponent } from './material-dialog-example.component';
 
@@ -8,16 +14,22 @@ describe('MaterialDialogExampleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MaterialDialogExampleComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        NgxTouchKeyboardModule,
+      ],
+      declarations: [MaterialDialogExampleComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MaterialDialogExampleComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the material dialog example', () => {
     expect(component).toBeTruthy();
   });
 });
