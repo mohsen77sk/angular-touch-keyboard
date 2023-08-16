729d6ff893a66dd699baf3e5eeda2c1b53e52e99
Mohsen <mohsen77sk@gmail.com>
2023-08-16 09:17:57 +0330

    fix formatting

--- projects/ngx-touch-keyboard/src/lib/Locale/ka-GE/index.ts
+++ projects/ngx-touch-keyboard/src/lib/Locale/ka-GE/index.ts
@@ -1,137 +1,138 @@
 import { Layout, Display, Locale } from '../type';
-
 import { fnDisplay } from '../constants';
 
 const layouts: Layout = {
-text_alphabetic: [
-[ 'ქ', 'წ', 'ე', 'რ', 'ტ', 'ყ', 'უ', 'ი', 'ო', 'პ' ],
-['ა', 'ს', 'დ', 'ფ', 'გ', 'ჰ', 'ჯ', 'კ', 'ლ'],
-['{shift}', 'ზ', 'ხ', 'ც', 'ვ', 'ბ', 'ნ', 'მ', '{backspace}'],
-['{numeric}', '{space}', '{done}'],
-],
-
-text_shift: [
-    ['ქ', 'ჭ', 'ე', 'ღ', 'თ', 'ყ', 'უ', 'ი', 'ო', 'პ'],
-    ['ა', 'შ', 'დ', 'ფ', 'გ', 'ჰ', 'ჟ', 'კ', '₾'],
-    ['{shift}', 'ძ', 'ხ', 'ჩ', 'ვ', 'ბ', 'ნ', 'მ', '{backspace}'],
-    ['{numeric}', '{space}', '{done}'],
-],
-text_numeric: [
-    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
-    ['-', '/', ':', ';', '(', ')', '$', '&', '@', '"'],
-    ['{symbolic}', '.', ',', '?', '!', "'", '{backspace}'],
-    ['{alphabetic}', '{space}', '{done}'],
-],
-text_symbolic: [
-    ['[', ']', '{', '}', '#', '%', '^', '*', '+', '='],
-    ['_', '\\', '|', '~', '<', '>', '€', '£', '¥', '•'],
-    ['{numeric}', '.', ',', '?', '!', "'", '{backspace}'],
-    ['{alphabetic}', '{space}', '{done}'],
-],
-search_alphabetic: [
-    [ 'ქ', 'წ', 'ე', 'რ', 'ტ', 'ყ', 'უ', 'ი', 'ო', 'პ' ],
+  text_alphabetic: [
+    ['ქ', 'წ', 'ე', 'რ', 'ტ', 'ყ', 'უ', 'ი', 'ო', 'პ'],
     ['ა', 'ს', 'დ', 'ფ', 'გ', 'ჰ', 'ჯ', 'კ', 'ლ'],
     ['{shift}', 'ზ', 'ხ', 'ც', 'ვ', 'ბ', 'ნ', 'მ', '{backspace}'],
     ['{numeric}', '{space}', '{done}'],
-],
-search_shift: [
+  ],
+  text_shift: [
     ['ქ', 'ჭ', 'ე', 'ღ', 'თ', 'ყ', 'უ', 'ი', 'ო', 'პ'],
     ['ა', 'შ', 'დ', 'ფ', 'გ', 'ჰ', 'ჟ', 'კ', '₾'],
     ['{shift}', 'ძ', 'ხ', 'ჩ', 'ვ', 'ბ', 'ნ', 'მ', '{backspace}'],
     ['{numeric}', '{space}', '{done}'],
-],
-search_numeric: [
+  ],
+  text_numeric: [
     ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     ['-', '/', ':', ';', '(', ')', '$', '&', '@', '"'],
     ['{symbolic}', '.', ',', '?', '!', "'", '{backspace}'],
     ['{alphabetic}', '{space}', '{done}'],
-],
-search_symbolic: [
+  ],
+  text_symbolic: [
     ['[', ']', '{', '}', '#', '%', '^', '*', '+', '='],
     ['_', '\\', '|', '~', '<', '>', '€', '£', '¥', '•'],
     ['{numeric}', '.', ',', '?', '!', "'", '{backspace}'],
     ['{alphabetic}', '{space}', '{done}'],
-],
-email_alphabetic: [
+  ],
+  search_alphabetic: [
+    ['ქ', 'წ', 'ე', 'რ', 'ტ', 'ყ', 'უ', 'ი', 'ო', 'პ'],
+    ['ა', 'ს', 'დ', 'ფ', 'გ', 'ჰ', 'ჯ', 'კ', 'ლ'],
+    ['{shift}', 'ზ', 'ხ', 'ც', 'ვ', 'ბ', 'ნ', 'მ', '{backspace}'],
+    ['{numeric}', '{space}', '{done}'],
+  ],
+  search_shift: [
+    ['ქ', 'ჭ', 'ე', 'ღ', 'თ', 'ყ', 'უ', 'ი', 'ო', 'პ'],
+    ['ა', 'შ', 'დ', 'ფ', 'გ', 'ჰ', 'ჟ', 'კ', '₾'],
+    ['{shift}', 'ძ', 'ხ', 'ჩ', 'ვ', 'ბ', 'ნ', 'მ', '{backspace}'],
+    ['{numeric}', '{space}', '{done}'],
+  ],
+  search_numeric: [
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    ['-', '/', ':', ';', '(', ')', '$', '&', '@', '"'],
+    ['{symbolic}', '.', ',', '?', '!', "'", '{backspace}'],
+    ['{alphabetic}', '{space}', '{done}'],
+  ],
+  search_symbolic: [
+    ['[', ']', '{', '}', '#', '%', '^', '*', '+', '='],
+    ['_', '\\', '|', '~', '<', '>', '€', '£', '¥', '•'],
+    ['{numeric}', '.', ',', '?', '!', "'", '{backspace}'],
+    ['{alphabetic}', '{space}', '{done}'],
+  ],
+  email_alphabetic: [
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
     ['{shift}', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '{backspace}'],
     ['{numeric}', '@', '{space}', '.', '{done}'],
-],
-email_shift: [
+  ],
+  email_shift: [
     ['a', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
     ['{shift}', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '{backspace}'],
     ['{numeric}', '@', '{space}', '.', '{done}'],
-],
-email_numeric: [
+  ],
+  email_numeric: [
     ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     ['$', '!', '~', '&', '=', '#', '[', ']'],
     ['{symbolic}', '.', '_', '-', '+', '{backspace}'],
     ['{alphabetic}', '@', '{space}', '.', '{done}'],
-],
-email_symbolic: [
+  ],
+  email_symbolic: [
     ['`', '|', '{', '}', '?', '%', '^', '*', '/', "'"],
     ['$', '!', '~', '&', '=', '#', '[', ']'],
     ['{numeric}', '.', '_', '-', '+', '{backspace}'],
     ['{alphabetic}', '@', '{space}', '.', '{done}'],
-],
-url_alphabetic: [
+  ],
+  url_alphabetic: [
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
     ['{shift}', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '{backspace}'],
     ['{numeric}', '/', '.com', '.', '{done}'],
-],
-url_shift: [
+  ],
+  url_shift: [
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
     ['{shift}', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '{backspace}'],
     ['{numeric}', '/', '.com', '.', '{done}'],
-],
-url_numeric: [
+  ],
+  url_numeric: [
     ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     ['@', '&', '%', '?', ',', '=', '[', ']'],
     ['{symbolic}', '_', ':', '-', '+', '{backspace}'],
     ['{alphabetic}', '/', '.com', '.', '{done}'],
-],
-url_symbolic: [
+  ],
+  url_symbolic: [
     ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     ['*', '$', '#', '!', "'", '^', '[', ']'],
     ['{numeric}', '~', ';', '(', ')', '{backspace}'],
     ['{alphabetic}', '/', '.com', '.', '{done}'],
-],
-numeric_default: [
+  ],
+  numeric_default: [
     ['1', '2', '3'],
     ['4', '5', '6'],
     ['7', '8', '9'],
     ['0', '{backspace}'],
-],
-decimal_default: [
+  ],
+  decimal_default: [
     ['1', '2', '3'],
     ['4', '5', '6'],
     ['7', '8', '9'],
     ['.', '0', '{backspace}'],
-],
-tel_default: [
+  ],
+  tel_default: [
     ['1', '2', '3', '*'],
     ['4', '5', '6', '#'],
     ['7', '8', '9', '+'],
     ['0', '{backspace}'],
-],
+  ],
 };
+
 const display: Display = {
-'{done}': fnDisplay.DONE,
-'{shift}': fnDisplay.SHIFT,
-'{backspace}': fnDisplay.BACKSPACE,
-'{space}': fnDisplay.SPACE,
-'{alphabetic}': 'აბგ',
-'{numeric}': '123',
-'{symbolic}': '#+=',
+  '{done}': fnDisplay.DONE,
+  '{shift}': fnDisplay.SHIFT,
+  '{backspace}': fnDisplay.BACKSPACE,
+  '{space}': fnDisplay.SPACE,
+  '{alphabetic}': 'აბგ',
+  '{numeric}': '123',
+  '{symbolic}': '#+=',
 };
+
 const locale: Locale = {
-code: 'ka-GE',
-dir: 'ltr',
-layouts: layouts,
-display: display,
+  code: 'ka-GE',
+  dir: 'ltr',
+  layouts: layouts,
+  display: display,
 };
+
 export default locale;
