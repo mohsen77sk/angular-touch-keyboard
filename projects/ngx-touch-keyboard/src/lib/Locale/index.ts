0d49534014f4849b309a0b612701bb15b361a302
avram <avram.eps@gmail.com>
2024-09-16 09:05:28 +0300

    add Hebrew language. (Israel)

--- projects/ngx-touch-keyboard/src/lib/Locale/index.ts
+++ projects/ngx-touch-keyboard/src/lib/Locale/index.ts
@@ -1,6 +1,7 @@
 export { default as enGB } from './en-GB';
 export { default as enUS } from './en-US';
 export { default as faIR } from './fa-IR';
+export { default as heIL } from './he-IL';
 export { default as kaGE } from './ka-GE';
 export { default as ruRU } from './ru-RU';
 export { default as svSE } from './sv-SE';
